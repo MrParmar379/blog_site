371b608402210f82343f38d4647ca4bb572dfa87
MrParmar379 <bhaveshparmar@fxis.ai>
2025-06-05 21:30:10 +0530

    model file added

---
+++ blog_site/blog/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import BlogType, BlogsTable
+
+admin.site.register(BlogType)
+admin.site.register(BlogsTable)
---
+++ blog_site/blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class BlogType(models.Model):
+    type_id = models.AutoField(primary_key=True)
+    type_title = models.CharField(max_length=50)
+    
+
+    def __str__(self):
+        return self.type_title
+
+class BlogsTable(models.Model):
+    blog_id = models.AutoField(primary_key=True)
+    content = models.TextField()
+    blog_type = models.ForeignKey(BlogType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
