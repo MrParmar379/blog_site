d099c32f20592e5f8888cec57ab05b169a5089e0
MrParmar379 <bhaveshparmar@fxis.ai>
2025-06-06 18:16:47 +0530

    code chnage in views file

--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -4,33 +4,53 @@ from rest_framework import status
 from django.shortcuts import get_object_or_404
 from .models import BlogsTable
 from .serializers import BlogSerializer
+from django.http import JsonResponse
 
 class BlogsTableAPIView(APIView):
-    def get(self, request, pk=None):
-        # if pk is not None:
-        #     blog = get_object_or_404(BlogsTable, pk=pk)
-        #     serializer = BlogSerializer(blog)
-        #     return Response(serializer.data)
+    def post(self, request):
+        try:
+            serializer = BlogSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return JsonResponse({'message': str(e)},"not creaed", status=status.HTTP_400_BAD_REQUEST)
+        
+
+        
+    def get (self, request, pk=None):
+        if pk is not None:
+            try:
+                blog = BlogsTable.objects.get(pk=pk)
+                serializer = BlogSerializer(blog)
+                return Response(serializer.data)
+            except BlogsTable.DoesNotExist:
+                return JsonResponse({'message': 'Blog not found'}, status=status.HTTP_404_NOT_FOUND) 
+                
         blogs = BlogsTable.objects.all()
         serializer = BlogSerializer(blogs, many=True)
         return Response(serializer.data)
-
-    def post(self, request):
-        serializer = BlogSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def put(self, request, pk):
-        blog = get_object_or_404(BlogsTable, pk=pk)
-        serializer = BlogSerializer(blog, data=request.data)
+    
+    def put(self, request, pk=None):
+        try:
+            blog = BlogsTable.objects.get(pk=pk)
+        except BlogsTable.DoesNotExist:
+            return JsonResponse({'message': 'Blog not found'}, status=status.HTTP_404_NOT_FOUND) 
+        serializer = BlogSerializer(blog, data=request.data)    
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        blog = get_object_or_404(BlogsTable, pk=pk)
+        return JsonResponse({'message': 'Blog not Update'}, status=status.HTTP_404_NOT_FOUND)
+    
+    def delete(self, request, pk=None):
+        try:
+            blog = BlogsTable.objects.get(pk=pk)
+        except BlogsTable.DoesNotExist:
+            return JsonResponse({'message': ' not found'}, status=status.HTTP_404_NOT_FOUND) 
         blog.delete()
-        return Response({'message': 'Blog deleted '}, status=status.HTTP_204_NO_CONTENT)
+        return JsonResponse({'message': ' Deleted'}, status=status.HTTP_404_NOT_FOUND)
+    
+
+    
+ 
--- blog_site/users/views.py
+++ blog_site/users/views.py
@@ -1,37 +1,50 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.shortcuts import get_object_or_404
 from .models import User
 from .serializers import UserSerializer
+from django.http import JsonResponse
 
 class UserTableAPIView(APIView):
     def get(self, request, pk=None):
         if pk is not None:
-            user = get_object_or_404(User, pk=pk)
-            serializer = UserSerializer(user)
-            return Response(serializer.data)
-        user = User.objects.all()
-        serializer = UserSerializer(user, many=True)
+            try:
+                user = User.objects.get(pk=pk)
+                serializer = UserSerializer(user)
+                return Response(serializer.data)
+            except User.DoesNotExist:
+                return JsonResponse({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND) 
+                    
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
-    
+
     def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            serializer = UserSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return JsonResponse({'message': str(e)},"not creaed", status=status.HTTP_400_BAD_REQUEST) 
     
+
     def put(self, request, pk):
-        user = get_object_or_404(User, pk=pk)
-        serializer = UserSerializer(user, data=request.data)
+        try:
+            user = User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            return JsonResponse({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND) 
+        serializer = UserSerializer(user, data=request.data)    
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+        return JsonResponse({'message': 'User not Update'}, status=status.HTTP_404_NOT_FOUND) 
+
     def delete(self, request, pk):
-        user = get_object_or_404(User, pk=pk)
+        try:
+            user = User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            return JsonResponse({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND) 
         user.delete()
-        return Response({'message': 'User deleted '}, status=status.HTTP_204_NO_CONTENT)
-        
+        return JsonResponse({'message': 'User Deleted'}, status=status.HTTP_404_NOT_FOUND) 
